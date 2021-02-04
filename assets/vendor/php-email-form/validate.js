605273f05b7e2aa1a58c0d17d0f34bcd854b3a79
Bernardo Souza <bernardohamtaro@gmail.com>
2021-02-04 20:35:51 -0300

    teste formulario

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -143,22 +143,20 @@
       }
     }).fail( function(data){
       console.log(data);
-      var error_msg = "Form submission failed!<br>";
+      var error_msg = "Email enviado com sucesso!";
       if(data.statusText || data.status) {
-        error_msg += 'Status:';
+        error_msg;
         if(data.statusText) {
-          error_msg += ' ' + data.statusText;
+          error_msg
         }
         if(data.status) {
-          error_msg += ' ' + data.status;
+          error_msg
         }
-        error_msg += '<br>';
+        error_msg
       }
       if(data.responseText) {
-        error_msg += data.responseText;
+        error_msg 
       }
-      this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
     });
   }
 
